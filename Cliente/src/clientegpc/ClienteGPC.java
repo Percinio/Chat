4df193da7889ab8e474b2474a1a80e225e03267d
Percinio <gabrielpercinioc@gmail.com>
2013-09-08 14:47:38 -0300

    adiciona função ola()

--- Cliente/src/clientegpc/ClienteGPC.java
+++ Cliente/src/clientegpc/ClienteGPC.java
@@ -8,9 +8,13 @@ import visualisacao.TelaAbertura;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;  
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;  
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import visualisacao.TelaPrincipal;
 
 /**
@@ -45,31 +49,68 @@ public class ClienteGPC {
        abreTelaDeAbertura();
     }
     
+    public static int checkSum(int servico, String dados) throws IOException {
+       
+        int checkSum=0;
+        int tamanhoDados;
+        int dadosProcessados = 0;
+        
+        tamanhoDados = dados.length() * 2;
+        
+        output.writeByte(servico);
+        System.out.println(servico);
+        output.writeByte(tamanhoDados);
+        System.out.println(tamanhoDados);
+        for (int count =0 ; count < dados.length(); count++) {
+            dadosProcessados += dados.charAt(count);
+            output.writeChar(dados.charAt(count));
+            System.out.println("Dado" + count + " :" + dados.charAt(count));
+            //System.out.println("Dado" + count + " :" + Integer.toHexString(dados.charAt(count)));
+        }
+        
+        checkSum += servico;
+        checkSum += tamanhoDados;
+        checkSum += dadosProcessados;
+        
+        output.writeByte(checkSum);
+        System.out.println(Integer.toHexString(checkSum));  
+
+        return checkSum;
+    }
+
+    
     public static void conecta(){
         try{                
-            socketCliente = new Socket(endereco,Integer.parseInt(porta));               
+            socketCliente = new Socket(endereco,Integer.parseInt(porta));
             input = new DataInputStream(socketCliente.getInputStream());  
             output = new DataOutputStream(socketCliente.getOutputStream());     
             fechaTelaDeAbertura();
             abreTelaPrincipal();
-            System.out.println(apelido);
+            servicoOla(apelido);
         }catch(IOException e){  
             System.out.println(e);
             System.out.println("Algum problema ocorreu ao criar ou enviar dados pelo socket.");        
         }
     }
+    
+    public static void servicoOla(String dadosApelido) {
+        try {
+            checkSum(1, dadosApelido);
+        } catch (IOException ex) {
+            System.out.println("Não foi possível mandar ola");
+            System.out.println(ex);
+        }
+    }
+    
     public static void setApelido(String Apelido) {
-        System.out.println(Apelido);
         ClienteGPC.apelido = Apelido;
     }
 
     public static void setEndereco(String Endereco) {
-        System.out.println(Endereco);
         ClienteGPC.endereco = Endereco;
     }
 
     public static void setPorta(String Porta) {
-        System.out.println(Porta);
         ClienteGPC.porta = Porta;
     }
     
